5504a96849557f0dc36c670a9dc3cf556e57dae3
Yeikdumm <95292743+Yeikdumm@users.noreply.github.com>
2025-10-23 09:49:42 -0500

    Delete public directory

--- public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require __DIR__ . '/../bootstrap/autoload.php';
-require __DIR__ . '/../bootstrap/helpers.php';
-require __DIR__ . '/../config/database.php';
-require_login();
-require __DIR__ . '/../app/Controllers/DashboardController.php';
-echo (new App\Controllers\DashboardController())->index();
--- public/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__ . '/../bootstrap/autoload.php';
-require __DIR__ . '/../bootstrap/helpers.php';
-require __DIR__ . '/../config/database.php';
-require __DIR__ . '/../app/Controllers/AuthController.php';
-echo (new App\Controllers\AuthController())->logout();
